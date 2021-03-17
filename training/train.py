e876ca56cfa2cadfb9d9c955a016159451510ffd
Sean Ashley <sean.d.ashley@gmail.com>
2021-03-16 23:31:19 -0400

    finished training code

--- training/train.py
+++ training/train.py
@@ -8,6 +8,7 @@ from sklearn.metrics import classification_report
 from xgboost import XGBClassifier
 import pickle
 from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import FunctionTransformer
 
 def load_data(data_path,test_size = 0.1,random_state = 42):
     """
@@ -49,8 +50,8 @@ def is_user_diabetic(avg_glucose_level):
     else:
         return False
 
-def add_diabetes(df):
-"""
+def add_diabetes(df,add_col = True):
+    """
     desc : creates and adds a diabetes column to the dataframe
 
     args:
@@ -58,11 +59,13 @@ def add_diabetes(df):
     return:
         df (pd.DataFrame) : stroke dataframe with diabetes column added
     """
-
-    stroke_data = df.copy()
-    stroke_data["is_user_diabetic"] =  stroke_data["avg_glucose_level"].apply(is_user_diabetic)
-    
-    return stroke_data
+    if add_col:
+        stroke_data = df.copy()
+        stroke_data["is_user_diabetic"] =  stroke_data["avg_glucose_level"].apply(is_user_diabetic)
+        
+        return stroke_data
+    #if we dont want to add the col return the same def
+    return df
 
 def bmi_to_bodytype(bmi):
     """
@@ -84,13 +87,43 @@ def bmi_to_bodytype(bmi):
     else:
         return "Obese"
 
-def add_bodytype(df):
+def add_bodytype(df, add_col = True):
     """
     desc : converts bmi to a category body type based on CDC guidelines https://www.cdc.gov/healthyweight/assessing/bmi/adult_bmi/index.html
     args:
         df (pd.DataFrame) : stroke dataframe
     returns:
         df (pd.DataFrame) : stroke dataframe with bodytype column
+    """
+
+    if add_col:
+        stroke_data = df.copy()
+        num_cols = stroke_data.select_dtypes(exclude=  ["object"])
+        num_cols_names = num_cols.columns
+        #impute missing values again to take into account new columns
+        imputer = SimpleImputer()
+
+        imputed_cols = imputer.fit_transform(num_cols)
+        imputed_cols = pd.DataFrame(data = imputed_cols,columns = num_cols.columns)
+
+
+        #apply function
+
+        stroke_data["body_type"] =  imputed_cols["bmi"].apply(bmi_to_bodytype)
+
+        
+        return stroke_data
+    #if we dont want to add the col the same df
+    return df
+
+def impute(df):
+    """
+    desc : imputes
+   
+    args:
+        df (pd.DataFrame) : stroke dataframe
+    returns:
+        df (pd.DataFrame) : stroke dataframe imputed
     """
     stroke_data = df.copy()
     num_cols = stroke_data.select_dtypes(exclude=  ["object"])
@@ -100,20 +133,10 @@ def add_bodytype(df):
 
     imputed_cols = imputer.fit_transform(num_cols)
     imputed_cols = pd.DataFrame(data = imputed_cols,columns = num_cols.columns)
-
-
-    #apply function
-
-    stroke_data["body_type"] =  imputed_cols["bmi"].apply(bmi_to_bodytype)
-
-    
     #drop numeric columns
     stroke_data.drop(columns = num_cols_names, axis = 1, inplace = True)
-
-    #add imputed columns
     stroke_data = pd.concat([stroke_data,imputed_cols], axis = 1)
     return stroke_data
-
 
 def one_hot_encode(df):
     """
@@ -136,4 +159,95 @@ def one_hot_encode(df):
     stroke_data = pd.concat([stroke_data,encoded_cols], axis = 1)
     return stroke_data
 
+def build_pipeline():
+    """
+    create the pipeline for the model, and optimize with gridsearch
+    """
+    #transform functions to make the pipeline work
+    one_hot_encode_transformed = FunctionTransformer(one_hot_encode)
+    impute_transformed = FunctionTransformer(impute)
+    add_bodytype_transformed = FunctionTransformer(add_bodytype)
+    add_diabetes_transformed = FunctionTransformer(add_diabetes)
 
+    pipeline = Pipeline([
+    
+    ("add_bodytype",add_bodytype_transformed),
+    ("add_diabetes",add_diabetes_transformed),
+    ("impute",impute_transformed),
+    ("one_hot_encode",one_hot_encode_transformed),
+    #use all available threads
+    ("pred",XGBClassifier(nthread = -1))
+    ])
+
+    #set up parameters to test
+    parameters = {
+        'pred__n_estimators': [50, 100, 200],
+        'pred__learning_rate': [0.01,0.1,0.3],
+        'add_bodytype__add_col' : [True,False],
+        'add_diabetes__add_col' : [True,False]
+    }
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
+
+def evaluate_model(model, X_test, y_test):
+    """
+    desc:   print out l1, recall, and precision
+            metrics for the model to evaluate performance
+    
+    args:
+        model (Gridsearchcv) : Trained model we are going to evaluate
+        X_test : X values used as validation to our model
+        y_test : y values used as validation to our model
+    returns:
+        None: Print out metrics (recall, precision, f1_score)
+    """
+    #make prediciton
+    y_pred = model.predict(X_test)
+    
+    #make classification
+    classification = classification_report(y_test,y_pred)
+
+    print(classification)
+
+
+def save_model(model, model_filepath):
+    """
+    desc : pickle the model for 
+    later use
+
+    args:
+        model (Gridsearchcv) : Trained model we are going to save
+        model_filepath (string) : Path to save the model
+    """
+    #pickle model
+    with open(model_filepath,"wb") as pickle_file:
+        pickle.dump(model,pickle_file)
+
+
+
+def main(database_filepath,model_filepath):
+    """
+    main function running
+    all necessary functions
+    """
+    X_train, X_test, y_train, y_test = load_data(database_filepath)
+    
+    print('Building model...')
+    model = build_pipeline()
+    
+    print('Training model...')
+    model.fit(X_train, y_train)
+    
+    print('Evaluating model...')
+    evaluate_model(model, X_test, y_test)
+
+    print('Saving model...')
+    save_model(model, model_filepath)
+
+    print('Trained model saved!')
+
+
+
+if __name__ == '__main__':
+    main("../data/healthcare-dataset-stroke-data.csv","model.pickle")
